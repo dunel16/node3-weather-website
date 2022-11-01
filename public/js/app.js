f7299d8d0dd57dac3abdbe877857fc032439df2c
unknown <dunel16@gmail.com>
2022-11-01 18:05:56 -0400

    Testing the new file

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,7 @@ const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    messageOne.textContent = 'Loading... '
+    messageOne.textContent = 'Loading... ' 
     messageTwo.textContent = ''
      fetch('http://localhost:3000/weather?address='+ search.value).then((response) => {
     response.json().then((data) => {
